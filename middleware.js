4f3360008c611b7e7d3f0117a00b5a6941310f0a
“kajalmapare” <kajalmapare24@gmail.com>
2025-04-10 23:55:11 +0530

    delete

--- middleware.js
+++ middleware.js
@@ -21,13 +21,23 @@ module.exports.saveRedirectUrl = (req, res, next) => {
 };
 
 module.exports.isOwner = async (req, res, next) => {
-  let { id } = req.params;
-  let listing = await Listing.findById(id);
-  if (!listing.owner._id.equals(res.locals.currUser._id)) {
-    req.flash("error", "You are not the owner of this listing");
-    return res.redirect(`/listings/${id}`);
+  try {
+    const { id } = req.params;
+    const listing = await Listing.findById(id).populate("owner");
+    if (!listing) {
+      req.flash("error", "Listing not found!");
+      return res.redirect("/listings");
+    }
+
+    if (!listing.owner._id.equals(req.user._id)) {
+      req.flash("error", "You don't have permission to do that!");
+      return res.redirect(`/listings/${id}`);
+    }
+    next();
+  } catch (err) {
+    req.flash("error", "Something went wrong!");
+    res.redirect("/listings");
   }
-  next();
 };
 
 module.exports.validateListing = (req, res, next) => {
